18aab7deefb35401d241abc1c52303ccda0cbdea
Mustafa Hammouz <mustafa947hammouz@gmail.com>
2019-02-24 16:37:47 +0200

    Updates

---
+++ precache-manifest.b06a915503ea2d1860d9579210b5c3c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "169e95adf4d0bcf0d173",
+    "url": "/mustafa94hammouz/react-select2/static/js/runtime~main.169e95ad.js"
+  },
+  {
+    "revision": "de3e2633f04227c600c7",
+    "url": "/mustafa94hammouz/react-select2/static/js/main.de3e2633.chunk.js"
+  },
+  {
+    "revision": "5ae20e8511f3b42a5a57",
+    "url": "/mustafa94hammouz/react-select2/static/js/2.5ae20e85.chunk.js"
+  },
+  {
+    "revision": "de3e2633f04227c600c7",
+    "url": "/mustafa94hammouz/react-select2/static/css/main.b447aaf9.chunk.css"
+  },
+  {
+    "revision": "a57c6a93ba2f720c52b0047a8b3d7619",
+    "url": "/mustafa94hammouz/react-select2/index.html"
+  }
+];
